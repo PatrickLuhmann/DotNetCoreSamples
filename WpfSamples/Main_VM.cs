e416bdc8326717d421098468fc9a0effda587ceb
Patrick Luhmann <patrick.luhmann@gmail.com>
2020-03-09 10:27:11 -0700

    Add Assets directory with sample text file
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ WpfSamples/Main_VM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WpfSamples
+{
+	public class Main_VM : INotifyPropertyChanged
+	{
+		private string _Salutation;
+		public string Salutation
+		{
+			get => _Salutation;
+			set
+			{
+				if (_Salutation != value)
+				{
+					_Salutation = value;
+					NotifyPropertyChanged("Salutation");
+				}
+			}
+		}
+
+		public Main_VM()
+		{
+			Salutation = "This is a temporary salutation.";
+
+			using (StreamReader read = new StreamReader("Assets/Salutation_Main.txt"))
+			{
+				Salutation = read.ReadToEnd();
+			}
+		}
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		#endregion
+	}
+}
