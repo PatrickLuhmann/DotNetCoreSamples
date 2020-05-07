a6395f6f12e381473bb58d8e13c5cd3d06fc8c9b
Patrick Luhmann <patrick.luhmann@gmail.com>
2020-05-07 14:18:38 -0700

    New Sample: Triggers And Styles
    This sample show how to use Trigger and DataTrigger to change the style of
    controls. This can be as simple as changing the background color to having
    only one of several overlapping controls be visible at one time.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ WpfSamples/Triggers/ViewModel/Trigger_VM.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WpfSamples.Triggers.ViewModel
+{
+	public class Trigger_VM : INotifyPropertyChanged
+	{
+		#region Properties
+		public string Title { get; set; }
+
+		public List<string> ComboList1 { get; set; }
+		private string _SelectedCombo1;
+		public string SelectedCombo1
+		{
+			get => _SelectedCombo1;
+			set
+			{
+				_SelectedCombo1 = value;
+				NotifyPropertyChanged("SelectedCombo1");
+			}
+		}
+
+		// Item C
+		private string _CDate;
+		public string CDate
+		{
+			get => _CDate;
+			set
+			{
+				_CDate = value;
+				NotifyPropertyChanged("CDate");
+			}
+		}
+
+		private string _CQuantity;
+		public string CQuantity
+		{
+			get => _CQuantity;
+			set
+			{
+				_CQuantity = value;
+				NotifyPropertyChanged("CQuantity");
+			}
+		}
+
+		private string _CAmount;
+		public string CAmount
+		{
+			get => _CAmount;
+			set
+			{
+				_CAmount = value;
+				NotifyPropertyChanged("CAmount");
+			}
+		}
+		#endregion
+
+		public Trigger_VM()
+		{
+			Title = "Welcome to the Trigger Sample";
+
+			ComboList1 = new List<string>
+			{
+				"Item A",
+				"Item B",
+				"Item C",
+			};
+			SelectedCombo1 = ComboList1[0];
+		}
+
+		#region NotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		#endregion
+	}
+}
